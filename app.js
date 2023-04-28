fbc53716bd70aa09fc5bf96ec23cdce312b13b4c
Closdlgdo <cidstp031921@gmail.com>
2023-04-28 06:06:19 -0700

    created the html, css and js for a basic drag and drop chess piece to different colors.

---
+++ app.js
@@ -0,0 +1,28 @@
+const king = document.querySelector('.chess-piece')
+
+const squares = document.querySelectorAll('.square')
+
+king.addEventListener('dragstart', dragStart)
+
+squares.forEach(square => {
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)})
+
+let beingDragged 
+
+function dragStart(e) {
+    beingDragged = e.target
+    console.log(beingDragged)
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragDrop(e) {
+    e.target.append(beingDragged)
+    console.log(e.target)
+}
+
+console.log(squares)
+//if there was more than one piece we would use querySelectorAll. 
