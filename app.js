c301cdc8fa05477ed8fcbb6112024c7b49614fd7
Closdlgdo <cidstp031921@gmail.com>
2023-04-29 05:57:51 -0700

    added five more squares, added pieces queen, pawn, and rook. also, they move around each square.

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-const king = document.querySelector('.chess-piece')
+const piece = document.querySelectorAll('.chess-piece')
 
 const squares = document.querySelectorAll('.square')
 
+const infoDisplay = document.querySelectorAll('#info')
+
+king.addEventListener('drag', dragging)
 king.addEventListener('dragstart', dragStart)
+
+queen.addEventListener('drag', dragging)
+queen.addEventListener('dragstart', dragStart)
+
+pawn.addEventListener('drag', dragging)
+pawn.addEventListener('dragstart', dragStart)
+
+rook.addEventListener('drag', dragging)
+rook.addEventListener('dragstart', dragStart)
 
 squares.forEach(square => {
     square.addEventListener('dragover', dragOver)
-    square.addEventListener('drop', dragDrop)})
+    square.addEventListener('dragenter', dragEnter)
+    square.addEventListener('dragleave', dragLeave)
+    square.addEventListener('drop', dragDrop),
+    square.addEventListener('dragend', dragEnd)
+})
 
 let beingDragged 
+
+function dragging() {
+    infoDisplay.textContent = 'You are dragging a ' + beingDragged.id
+}
 
 function dragStart(e) {
     beingDragged = e.target
@@ -19,9 +39,24 @@ function dragOver(e) {
     e.preventDefault()
 }
 
+function dragEnter(e) {
+    e.target.classList.add('highlight')
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('highlight')
+}
+
 function dragDrop(e) {
     e.target.append(beingDragged)
+    e.target.classList.remove('highlight')
     console.log(e.target)
+}
+
+function dragEnd(e) {
+    e.target.classList.add('target')
+    setTimeout(() => e.target.classList.remove('target'), 100)
+    infoDisplay.textContent = 'You have dropped a ' + beingDragged.id
 }
 
 console.log(squares)
